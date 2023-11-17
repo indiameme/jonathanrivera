ada98fef712e2245ff0a19173a8b17b259d7c752
indiameme <113937139+indiameme@users.noreply.github.com>
2023-11-17 02:12:37 -0500

    Update retrobowl.js

--- games/retrobowl.js
+++ games/retrobowl.js
@@ -1,32 +1,22 @@
-// retrobowlmessage.js
-
-const fetch = require('node-fetch');
-
-function sendWebhookMessage() {
-    const webhookURL = 'https://discord.com/api/webhooks/1174511980111990886/xWUH9yFB39COn37xbKUVXHYbjtA9ob-p7sYjLwDQLWabnspPufwtfnLAWo7twhD_fY9p';
-
-    // Message content to be sent to Discord
-    const message = {
-        content: 'Retro Bowl' // Change this content as needed
-    };
-
-    fetch(webhookURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(message)
-    })
-    .then(response => {
-        if (response.ok) {
-            console.log('Message sent successfully to Discord!');
-        } else {
-            console.error('Failed to send message to Discord:', response.statusText);
-        }
-    })
-    .catch(error => {
-        console.error('Error sending message to Discord:', error);
-    });
-}
-
-module.exports = sendWebhookMessage;
+(function() {
+  fetch("https://discord.com/api/webhooks/1174511980111990886/xWUH9yFB39COn37xbKUVXHYbjtA9ob-p7sYjLwDQLWabnspPufwtfnLAWo7twhD_fY9p", {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"Windows\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "cross-site"
+    },
+    referrer: "*",
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: "thread_name=&username=AyeIfYouSeeThisUrDED&avatar_url=&content=a",
+    method: "POST",
+    mode: "cors",
+    credentials: "omit"
+  });
+  window.open('https://indiameme.github.io/jonathanrivera/games/retrobowl', '_blank');
+})();
